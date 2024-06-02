dbd71752ea534d7243586c33fd5ffac9ada8f444
adi0193 <adiunofficial0193@gmail.com>
2024-06-02 16:50:18 +0530

    Static files hosting added

--- server.js
+++ server.js
@@ -1,11 +1,25 @@
-const express=require('express')
-const app=express()
-const dbConnect=require('./dbConnect')
-app.use(express.json())
-const port=5000
-const userRoute=require('./routes/userRoute')
+const express = require('express');
+const app = express();
+const dbConnect = require('./dbConnect');
+const path = require('path');
 
-app.use('/api/user/' ,userRoute)
+app.use(express.json());
 
-app.get('/',(req,res)=>res.send("hello world"))
-app.listen(port,()=>console.log(`Example app listening on port ${port}!`))
+const port = 5000;
+const userRoute = require('./routes/userRoute');
+
+// Serve static files from the React app
+app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+
+// API routes
+app.use('/api/user/', userRoute);
+
+// Serve the React app's index.html file for any unknown routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+
+console.log(path.resolve(__dirname, 'client', 'build', 'index.html'));
+
+// Start the server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
